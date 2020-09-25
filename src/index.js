a6bf6a42096acec26e558ac253cf204f401a3ebe
Shaun Wu <Alexbay218@gmail.com>
2020-09-25 00:05:55 -0400

    Delete old engin

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import 'index.css';
 import App from 'App';
 import * as serviceWorker from 'serviceWorker';
 
-require('module-alias/register');
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
