af234d458a7ac0024d1535d28a9aed2af1f554b1
Shaun Wu <Alexbay218@gmail.com>
2020-10-04 19:30:09 -0400

    Move service worker functions to component

--- src/components/UpdateToast.js
+++ src/components/UpdateToast.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 import { withSnackbar } from 'notistack';
+import * as serviceWorker from 'serviceWorker';
+
+const swUpdateAvailable = new Event('swupdateavailable');
+const swOfflineReady = new Event('swofflineready');
 
 class UpdateToast extends React.Component {
   updateToast() {
@@ -14,6 +18,14 @@ class UpdateToast extends React.Component {
     this.offlineToast = this.offlineToast.bind(this);
     window.addEventListener('swupdateavailable', this.updateToast);
     window.addEventListener('swofflineready', this.offlineToast);
+    serviceWorker.register({
+      onUpdate: () => { window.dispatchEvent(swUpdateAvailable); },
+      onSuccess: () => { window.dispatchEvent(swOfflineReady); }
+    });
+  }
+  componentWillUnmount() {
+    window.removeEventListener('swupdateavailable', this.updateToast);
+    window.removeEventListener('swofflineready', this.offlineToast);
   }
   render() { return <></>; }
 }
