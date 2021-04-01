d9322c3ee203efc56100a097410d1ceb999d2951
Shaun Wu <alexbay218@gmail.com>
2021-04-01 19:21:08 -0400

    Init ui overhaul

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,25 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
 
-import { ThemeProvider } from 'emotion-theming';
-import theme from '@rebass/preset';
-import { SnackbarProvider } from 'notistack';
-import { Howler } from 'howler';
-
-import Menu from 'uiTree/Menu';
-import Game from 'uiTree/Game';
-import Tutorial from 'uiTree/Tutorial';
-import UpdateToast from 'components/UpdateToast';
-import Music from 'components/Music';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 import 'fontsource-roboto';
 import 'App.css';
 
 export default class App extends React.Component {
-  resizeListener = () => {
-    this.forceUpdate();
-  };
-  visibilityListener = () => {
-    const state = document.visibilityState;
-    if(state === "hidden") {
-      Howler.mute(true);
-    }
-    if(state === "visible") {
-      Howler.mute(false);
-    }
-  };
-  componentDidMount() {
-    this.resizeListener = () => {
-      this.forceUpdate();
+  constructor() {
+    this.state = {
+      muiTheme: {}
     };
-    window.addEventListener('resize', this.resizeListener);
-    window.addEventListener('visibilitychange', this.visibilityListener);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.resizeListener);
-    window.removeEventListener('visibilitychange', this.visibilityListener);
   }
   render() {
     return (
       <HashRouter>
-        <ThemeProvider theme={theme}>
-          <SnackbarProvider maxSnack={2}>
-            <UpdateToast />
-            <Music />
-            <Menu />
-            <Game />
-            <Tutorial />
-          </SnackbarProvider>
-        </ThemeProvider>
+        <CssBaseline>
+          <ThemeProvider theme={createMuiTheme(this.state.muiTheme)}>
+          </ThemeProvider>
+        </CssBaseline>        
       </HashRouter>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
 
-import 'index.css';
 import App from 'App';
-
-Modal.setAppElement('#root');
 
 ReactDOM.render(
   <React.StrictMode>
