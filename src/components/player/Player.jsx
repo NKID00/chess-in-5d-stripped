0a528b4c9f832099fc2b2c70837ded5fbb2865f3
Shaun Wu <alexbay218@gmail.com>
2021-04-26 00:05:52 -0400

    layout transform mode

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -10,6 +10,7 @@ import Renderer from 'components/player/Renderer';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
+const deepcopy = require('deepcopy');
 const store = require('store');
 
 /*
@@ -21,6 +22,9 @@ Props:
  - availableMoves
  - pastAvailableMoves
 */
+const rowHeight = 30;
+const rowMargin = 10;
+
 export default class Player extends React.Component {
   rootRef=React.createRef();
   chessRenderer = React.createRef();
@@ -31,33 +35,110 @@ export default class Player extends React.Component {
   defaultLayout() {
     return {
       lg: [
-        {i: 'clock', x: 4, y: 2, w: 4, h: 2}
+        {i: 'menu', x: 0, y: 2, w: 4, h: 2},
+        {i: 'clock', x: 4, y: 2, w: 4, h: 2},
+        {i: 'analyze', x: 4, y: 4, w: 4, h: 2},
+        {i: 'tutorial', x: 8, y: 2, w: 4, h: 9},
+        {i: 'settings', x: 0, y: 4, w: 4, h: 7},
+        {i: 'notation', x: 0, y: -9, w: 4, h: 9},
+        {i: 'view', x: 6, y: -2, w: 3, h: 2},
+        {i: 'submit', x: 9, y: -2, w: 3, h: 2},
       ],
       md: [
-        {i: 'clock', x: 3, y: 2, w: 4, h: 2}
+        {i: 'menu', x: 0, y: 2, w: 4, h: 2},
+        {i: 'clock', x: 4, y: 2, w: 4, h: 2},
+        {i: 'analyze', x: 4, y: 4, w: 4, h: 2},
+        {i: 'tutorial', x: 8, y: 2, w: 4, h: 9},
+        {i: 'settings', x: 0, y: 4, w: 4, h: 7},
+        {i: 'notation', x: 0, y: -9, w: 5, h: 9},
+        {i: 'view', x: 6, y: -2, w: 3, h: 2},
+        {i: 'submit', x: 9, y: -2, w: 3, h: 2},
       ],
       sm: [
-        {i: 'clock', x: 2, y: 2, w: 2, h: 2}
+        {i: 'menu', x: 0, y: 2, w: 6, h: 2},
+        {i: 'clock', x: 6, y: 2, w: 6, h: 2},
+        {i: 'analyze', x: 6, y: 4, w: 6, h: 2},
+        {i: 'tutorial', x: 7, y: -9, w: 5, h: 7},
+        {i: 'settings', x: 0, y: 4, w: 4, h: 6},
+        {i: 'notation', x: 0, y: -7, w: 4, h: 7},
+        {i: 'view', x: 4, y: -2, w: 4, h: 2},
+        {i: 'submit', x: 8, y: -2, w: 4, h: 2},
       ],
       xs: [
-        {i: 'clock', x: 1, y: 2, w: 2, h: 2}
+        {i: 'menu', x: 0, y: 2, w: 6, h: 2},
+        {i: 'clock', x: 6, y: 2, w: 6, h: 2},
+        {i: 'analyze', x: 6, y: 4, w: 6, h: 2},
+        {i: 'tutorial', x: 6, y: -9, w: 6, h: 7},
+        {i: 'settings', x: 0, y: 4, w: 4, h: 6},
+        {i: 'notation', x: 0, y: -9, w: 6, h: 7},
+        {i: 'view', x: 2, y: -2, w: 5, h: 2},
+        {i: 'submit', x: 7, y: -2, w: 5, h: 2},
       ],
       xxs: [
-        {i: 'clock', x: 0, y: 2, w: 2, h: 2}
+        {i: 'menu', x: 0, y: 2, w: 6, h: 2},
+        {i: 'clock', x: 6, y: 2, w: 6, h: 2},
+        {i: 'analyze', x: 0, y: 4, w: 12, h: 2},
+        {i: 'tutorial', x: 0, y: -7, w: 12, h: 5},
+        {i: 'settings', x: 0, y: 6, w: 5, h: 5},
+        {i: 'notation', x: 5, y: 6, w: 7, h: 6},
+        {i: 'view', x: 0, y: -2, w: 6, h: 2},
+        {i: 'submit', x: 6, y: -2, w: 6, h: 2},
       ],
     };
   }
   layouts() {
     if(typeof store.get('player/layouts') === 'object') {
-      return store.get('player/layouts');
+      return this.transformLayout(store.get('player/layouts'));
     }
-    return this.defaultLayout();
+    return this.transformLayout(this.defaultLayout());
   }
   resize() {
     this.rootRef.current.style.width = `${window.innerWidth}px`;
     this.rootRef.current.style.height = `${window.innerHeight}px`;
     this.chessRenderer.current.refreshAttach();
-    this.setState({ width: window.innerWidth });
+    this.setState({
+      width: window.innerWidth,
+      layouts: this.layouts()
+    });
+  }
+  transformLayout(layout, importMode = true) {
+    var res = deepcopy(layout);
+    var defaultLayout = this.defaultLayout();
+    var maxHeight = Math.floor(window.innerHeight / (rowHeight + rowMargin));
+    var keys = Object.keys(res);
+    //Merge from defaultLayout
+    for(var j = 0;j < keys.length;j++) {
+      for(var k = 0;k < defaultLayout[keys[j]].length;k++) {
+        var includesItem = false;
+        for(var i = 0;i < res[keys[j]].length;i++) {
+          if(res[keys[j]][i].i === defaultLayout[keys[j]][k].i) {
+            includesItem = true;
+          }
+        }
+        if(!includesItem) {
+          res[keys[j]].push(defaultLayout[keys[j]][k]);
+        }
+      }
+    }
+    if(importMode) {
+      for(var j = 0;j < keys.length;j++) { // eslint-disable-line no-redeclare
+        for(var i = 0;i < res[keys[j]].length;i++) { // eslint-disable-line no-redeclare
+          if(res[keys[j]][i].y < 0) {
+            res[keys[j]][i].y = maxHeight + res[keys[j]][i].y;
+          }
+        }
+      }
+    }
+    else {
+      for(var j = 0;j < keys.length;j++) { // eslint-disable-line no-redeclare
+        for(var i = 0;i < res[keys[j]].length;i++) { // eslint-disable-line no-redeclare
+          if(res[keys[j]][i].y > maxHeight / 2) {
+            res[keys[j]][i].y = -(maxHeight - res[keys[j]][i].y);
+          }
+        }
+      }
+    }
+    return res;
   }
   componentDidMount() {
     this.resize();
@@ -75,6 +156,7 @@ export default class Player extends React.Component {
           position: 'absolute',
           top: 0,
           left: 0,
+          overflow: 'hidden'
         }}
       >
         <div
@@ -106,19 +188,41 @@ export default class Player extends React.Component {
           <ResponsiveGridLayout
             width={this.state.width}
             breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
-            cols={{lg: 12, md: 10, sm: 6, xs: 4, xxs: 2}}
+            cols={{lg: 12, md: 12, sm: 12, xs: 12, xxs: 12}}
             compactType={null}
-            rowHeight={30}
+            margin={[rowMargin,rowMargin]}
+            rowHeight={rowHeight}
             layouts={this.state.layouts}
             onLayoutChange={(l,ls) => {
-              store.set('player/layouts', ls);
+              store.set('player/layouts', this.transformLayout(ls, false));
               this.setState({
                 layouts: this.layouts()
               });
             }}
           >
+            <Card key='menu'>
+              <CardContent>Menu</CardContent>
+            </Card>
             <Card key='clock'>
               <CardContent>Clock</CardContent>
+            </Card>
+            <Card key='analyze'>
+              <CardContent>Analyze</CardContent>
+            </Card>
+            <Card key='tutorial'>
+              <CardContent>Tutorial</CardContent>
+            </Card>
+            <Card key='settings'>
+              <CardContent>Settings</CardContent>
+            </Card>
+            <Card key='notation'>
+              <CardContent>Notation</CardContent>
+            </Card>
+            <Card key='view'>
+              <CardContent>View</CardContent>
+            </Card>
+            <Card key='submit'>
+              <CardContent>Submit</CardContent>
             </Card>
           </ResponsiveGridLayout>
         </div>
