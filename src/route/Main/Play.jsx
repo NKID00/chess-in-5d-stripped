7794723749627c151fafd0fa458024678d1ff482
Shaun Wu <alexbay218@gmail.com>
2021-05-11 23:14:05 -0400

    Fix player folder (2/2)

--- src/route/Main/Play.jsx
+++ src/route/Main/Play.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Player from 'components/Player2';
+import Player from 'components/Player';
 
 import Chess from '5d-chess-js';
 
--- src/route/Main/Settings/Theme.jsx
+++ src/route/Main/Settings/Theme.jsx
@@ -23,7 +23,7 @@ import TextField from '@material-ui/core/TextField';
 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-import Clock from 'components/Player2/Clock';
+import Clock from 'components/Player/Clock';
 import ColorPicker from 'components/ColorPicker';
 
 import EmitterContext from 'EmitterContext';
