dd8a479270148c96c455e3ab3c67732cf3f1e2c4
Shaun Wu <alexbay218@gmail.com>
2021-07-17 03:07:49 -0400

    Add support for 5d-chess-workspaces

--- src/state/i18n.js
+++ src/state/i18n.js
@@ -1,6 +1,6 @@
 import { i18n } from '@lingui/core';
-import { messages as enMessages } from 'locales/en/messages.js';
-import { messages as frMessages } from 'locales/fr/messages.js';
+import { messages as enMessages } from '@lingui/loader!locales/en/messages.po'; // eslint-disable-line import/no-webpack-loader-syntax
+import { messages as frMessages } from '@lingui/loader!locales/fr/messages.po'; // eslint-disable-line import/no-webpack-loader-syntax
 
 //Local for converse.js
 import 'converse.js/dist/locales/fr-LC_MESSAGES-converse-po';
